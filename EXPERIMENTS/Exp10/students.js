f51824062b130dacbaef3e25743819de4a342b48
sharwaributle <sharwaributle@gmail.com>
2026-09-15 10:08:05 +0530

    Add student data to students.js
    Added a list of student objects with details such as ID, roll number, name, course, year, email, and marks.

---
+++ EXPERIMENTS/Exp10/students.js
@@ -0,0 +1,47 @@
+[
+  {
+    "id": 101,
+    "rollNo": 24070521228,
+    "name": "Sharwari Butle",
+    "course": "B.Tech Computer Science",
+    "year": "3rd Year",
+    "email": "sharwari@example.com",
+    "marks": 87
+  },
+  {
+    "id": 102,
+    "rollNo": 24070521231,
+    "name": "Nandini Durugkar",
+    "course": "B.Tech Information Technology",
+    "year": "3rd Year",
+    "email": "nandini@example.com",
+    "marks": 91
+  },
+  {
+    "id": 103,
+    "rollNo": 24070521215,
+    "name": "Anuhska Paliwal",
+    "course": "B.Tech Computer Science",
+    "year": "2nd Year",
+    "email": "anuhska@example.com",
+    "marks": 78
+  },
+  {
+    "id": 104,
+    "rollNo": 24070521236,
+    "name": "Esha Hadap",
+    "course": "B.Tech Electronics",
+    "year": "3rd Year",
+    "email": "esha@example.com",
+    "marks": 84
+  },
+  {
+    "id": 105,
+    "rollNo": 24070521229,
+    "name": "Kunali Dahikar",
+    "course": "B.Tech Computer Science",
+    "year": "2nd Year",
+    "email": "kunali@example.com",
+    "marks": 93
+  }
+]
